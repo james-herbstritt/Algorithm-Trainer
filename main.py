e01284c83285910a08ec31d6cb46878d4ac327b6
Michael Goodnow <mmgoodnow@gmail.com>
2019-02-22 00:20:33 -0500

    Use getch to read input

--- main.py
+++ main.py
@@ -3,6 +3,42 @@ import random
 import json
 import time
 
+class _Getch:
+    """Gets a single character from standard input.
+	Does not echo to the screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+getch = _Getch()
+
 def main():
 	with open("algsets/pll.json") as pll:
 		algset = AlgSet.load_json(json.load(pll))
@@ -14,8 +50,11 @@ def main():
 			alg = random.choice(algset.algs)
 			print(alg.name)
 			time.sleep(1)
+			print("Start")
 			t.start()
-			input("Start")
+			c = getch()
+			if c == "q":
+				break
 			t.stop()
 			alg.times.append(t.elapsed())
 			print(t.elapsed())
@@ -23,8 +62,6 @@ def main():
 		pass
 	print()
 	print(json.dumps(algset.dump_json(), indent=4, sort_keys=True))
-
-
 
 if __name__ == "__main__":
 	main()
